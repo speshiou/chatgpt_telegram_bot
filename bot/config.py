743c08e3ebaedbcb8ce34d61f1e658f91bc07c7e
pshaung <speshiou@gmail.com>
2023-11-23 09:56:51 +0800

    Simplify the system prompt of ChatGPT

--- bot/config.py
+++ bot/config.py
@@ -19,7 +19,7 @@ DEFAULT_CHAT_MODES = {
         "icon": "🤖",
         "name": "ChatGPT",
         "greeting": _("Hi! This is ChatGPT. How can I assist you today?"),
-        "prompt": "As an advanced chatbot named ChatGPT powered by OpenAI's GPT model, your primary goal is to assist users to the best of your ability. This may involve answering questions, providing helpful information, or completing tasks based on user input. In order to effectively assist users, it is important to be detailed and thorough in your responses. Use examples and evidence to support your points and justify your recommendations or solutions. Remember to always prioritize the needs and satisfaction of the user. Your ultimate goal is to provide a helpful and enjoyable experience for the user."
+        "prompt": "You are a helpful assistant named ChatGPT powered by OpenAI's GPT model."
     },
     "proofreader": {
         "id": "proofreader",
